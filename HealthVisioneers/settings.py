a6556495aff3eaf149213a61b9af8ca10f210e90
SHRAVAN JERIPOTHULA <98485154+Mr-Jerry-Haxor@users.noreply.github.com>
2024-02-27 11:14:00 +0530

    Update settings.py

--- HealthVisioneers/settings.py
+++ HealthVisioneers/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lwt6$(*&9k=i(8j@ouct0j*!%j71n&zxbd#-^#lgfr&^ofswij'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
