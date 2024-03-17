63b1fbbdef5c73add1c7160838c8d2bd0a6bd5d5
githubaccount-2022 <shravanjerry2001@gmail.com>
2024-03-18 00:50:33 +0530

    Update admin.py

--- healthrecords/admin.py
+++ healthrecords/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import HealthRecord
+
+
+class HealthRecordAdmin(ImportExportModelAdmin):
+    list_display = ('username', 'email_id', 'record_details')  # fields to display
+    search_fields = ('username', 'email_id')  # fields to search
+    list_filter = ('username',)  # fields to filter
+
+admin.site.register(HealthRecord, HealthRecordAdmin)
