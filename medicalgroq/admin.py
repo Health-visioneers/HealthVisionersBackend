6b53741f9e2ae2f1168791b018c85b28fadb1b49
SHRAVAN JERIPOTHULA <98485154+Mr-Jerry-Haxor@users.noreply.github.com>
2024-03-18 14:50:31 +0530

    Update admin.py

--- medicalgroq/admin.py
+++ medicalgroq/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import ChatMessage
+
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'content', 'timestamp')
+    search_fields = ('user__username', 'content',)
+
+admin.site.register(ChatMessage, ChatMessageAdmin)
